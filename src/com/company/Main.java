63ceab3534ecea9a9bea0b548de3e47e2e2dc186
lolaorifjonova <lorifjonova@gmail.com>
2020-11-12 11:30:32 -0500

    started initial work

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,8 +4,8 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.println("Hello Git Last Day!");
-//   B21 is excited !!!
-        //  B21  Successful testers!!
-        //  b21 is dedicated!!!!
+        //   B21 is excited !!!
+        //   B21  Successful testers!!
+        //   B21 is dedicated!!!!
     }
 }
